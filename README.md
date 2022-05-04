6ed1a74d8e17ffa815002786d454d018ccf7ab24
Adam <49234582+Natixe@users.noreply.github.com>
2022-05-04 12:21:52 +0200

    v0.0.0

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Better-overlap
 
 
-| Title  | Version | License | Link |
-| ------------- | ------------- | ------------- | ------------- |
-| Better-overlap  | 1.18.2  | CC BY-NC-SA 4.0 | https://creativecommons.org/licenses/by-nc-sa/4.0/ |
+| Title  | Version           | License | Link |
+| ------------- |-------------------| ------------- | ------------- |
+| Better-overlap  | 1.18.2 And 1.16.5 | CC BY-NC-SA 4.0 | https://creativecommons.org/licenses/by-nc-sa/4.0/ |
+
+
+| Version  | Statue         |
+|----------|----------------|
+| 1.18.2   | In development |
+| 1.16.5   | In development |
--- src/main/java/fr/natixe/betteroverlap/BO.java
+++ src/main/java/fr/natixe/betteroverlap/BO.java
@@ -1,86 +1,78 @@
-package com.example.examplemod;
+package fr.natixe.betteroverlap;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+@Mod(BO.MODID)
+public class BO {
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("BO.MODID")
-public class BO
-{
-    public static final String MODID = "bo";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+  public static final String MODID = "bo";
+  // Directly reference a log4j logger.
+  private static final Logger LOGGER = LogManager.getLogger();
 
-    public BO()
-    {
-      //IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-//
-      //ModItems.register(bus);
-      //ModBlocks.register(bus);
-      // Register the setup method for modloading
-      FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-      // Register the enqueueIMC method for modloading
-      FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-      // Register the processIMC method for modloading
-      FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+  public BO() {
+    IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-      // Register ourselves for server and other game events we are interested in
-      MinecraftForge.EVENT_BUS.register(this);
-    }
+    //ModItems.register(bus);
+    //ModBlocks.register(bus);
+    bus.addListener(this::setup);
+    // Register the enqueueIMC method for modloading
+    bus.addListener(this::enqueueIMC);
+    // Register the processIMC method for modloading
+    bus.addListener(this::processIMC);
+    // Register the doClientStuff method for modloading
+    bus.addListener(this::doClientStuff);
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+    // Register ourselves for server and other game events we are interested in
+    MinecraftForge.EVENT_BUS.register(this);
+  }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
+  private void setup(final FMLCommonSetupEvent event) {
 
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // Some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.messageSupplier().get()).
-                collect(Collectors.toList()));
-    }
+  }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+  private void doClientStuff(final FMLClientSetupEvent event) {
+
+  }
+
+  private void enqueueIMC(final InterModEnqueueEvent event) {
+    // some example code to dispatch IMC to another mod
+    InterModComms.sendTo("examplemod", "helloworld", () -> {
+      LOGGER.info("Hello world from the MDK");
+      return "Hello world";
+    });
+  }
+
+  private void processIMC(final InterModProcessEvent event) {
+
+  }
+
+  @SubscribeEvent
+  public void onServerStarting(FMLServerStartingEvent event) {
+    // do something when the server starts
+    LOGGER.info("HELLO from server starting");
+  }
+
+
+  @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+  public static class RegistryEvents {
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+      // register a new block here
+      LOGGER.info("HELLO from Register Block");
     }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents
-    {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
-        {
-            // Register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+  }
 }
